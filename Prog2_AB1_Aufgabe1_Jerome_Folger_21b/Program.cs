62105b0a05db73c4e157e290b1a1c5d360c087f0
folger-j <jerome.folger@gmail.com>
2023-02-10 10:54:24 +0100

    Add project files.

---
+++ Prog2_AB1_Aufgabe1_Jerome_Folger_21b/Program.cs
@@ -0,0 +1,8 @@
+﻿using Prog2_AB1_Aufgabe1_Jerome_Folger_21b;
+using System.Reflection.PortableExecutable;
+
+Umfrage umfrage = new Umfrage();
+
+umfrage.StelleFrage("Frage 1", "Wie heisst dein Haustier?");
+umfrage.StelleFrage("Frage 1", "Wie heisst dein Haustier?", true);
+umfrage.StelleFrage("Frage 1", "Wie heisst dein Haustier?", "Bitte Eingabe machen", "^[a-z]+$");
---
+++ Prog2_AB1_Aufgabe1_Jerome_Folger_21b/Umfrage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2_AB1_Aufgabe1_Jerome_Folger_21b
+{
+    internal class Umfrage
+    {
+        public void StelleFrage(string id, string frage)
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(frage);
+        }
+        public void StelleFrage(string id, string frage, bool required)
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(frage);
+            if(required) Console.WriteLine("Pflichtfeld");
+        }
+        public void StelleFrage(string id, string frage, string instruction, string regexpr)
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(frage);
+            Console.WriteLine(instruction);
+            string antwort = Console.ReadLine() ?? "";
+            var m = Regex.Match(antwort, regexpr);
+            if (m.Success)
+            {
+                Console.WriteLine("Antwort: " + antwort);
+            }
+            else
+            {
+                Console.WriteLine("Fehler: Eingabe nicht erkannts");
+            }
+        }
+    }
+}
